2803ba23a8642b774b1f6f463b02e0a044483f7b
shagyrovmaksat <60479259+shagyrovmaksat@users.noreply.github.com>
2021-04-20 23:01:12 +0500

    icons

--- foodie/foodie/LogInViewController.swift
+++ foodie/foodie/LogInViewController.swift
@@ -9,13 +9,22 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.logInButton.layer.cornerRadius = 5
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func logInPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
