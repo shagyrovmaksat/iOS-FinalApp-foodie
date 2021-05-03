d6babd35ef7780676f2bb08398e47de8da407ce8
shagyrovmaksat <60479259+shagyrovmaksat@users.noreply.github.com>
2021-05-03 20:36:58 +0500

    edit and detail info recipe

---
+++ foodie/foodie/User/EditRecipeVC.swift
@@ -0,0 +1,29 @@
+//
+//  EditRecipeVC.swift
+//  foodie
+//
+//  Created by Shagirov Maksat on 03.05.2021.
+//
+
+import UIKit
+
+class EditRecipeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
